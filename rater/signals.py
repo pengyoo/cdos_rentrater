fb2d01a77741b47c3a0161dd990a4d8d18f52ee7
Peng Yu <yp1990@gmail.com>
2024-03-09 19:59:02 +0000

    calculate average rating when a user review on a property using signal

--- rater/signals.py
+++ rater/signals.py
@@ -1,7 +1,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import UserProfile, Claim, Property
+from django.db.models import Avg
+
+from .models import UserProfile, Claim, Property, Review
 
 
 # Use django signals to automatically create UserProfile when a user sign up
@@ -10,12 +12,21 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
+
 # Use signals to associate property with a landlord when it's verified by admin user
-
-
 @receiver(post_save, sender=Claim)
 def create_profile(sender, instance, created, **kwargs):
     if instance.verified:
         property = Property.objects.get(pk=instance.property.id)
         property.landlord = instance.user
         property.save()
+
+
+# Use signals to calculate average rating when a user rate a property
+@receiver(post_save, sender=Review)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        avg_rating = Review.objects.filter(property=instance.property).aggregate(
+            avg_rating=Avg('rating'))['avg_rating']
+        Property.objects.filter(
+            pk=instance.property.id).update(rating=avg_rating)
