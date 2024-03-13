26d2ba66657db196cc05d8dc2d22d120fd5d7931
Peng Yu <yp1990@gmail.com>
2024-03-13 19:35:00 +0000

    custimize display fields in admin system

--- rater/admin.py
+++ rater/admin.py
@@ -1,10 +1,50 @@
 from django.contrib import admin
 from . import models
+from django.utils.html import format_html
 
 # Register models.
 admin.site.register(models.UserProfile)
-admin.site.register(models.Image)
-admin.site.register(models.Property)
-admin.site.register(models.Review)
-admin.site.register(models.Reply)
 admin.site.register(models.Claim)
+
+# Custimize display fields for Image model
+
+
+@admin.register(models.Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'image']
+
+    def image(self, image):
+        return format_html('<img src="{}" class="thumbnail"/>',
+                           image.url)
+
+    class Media:
+        css = {
+            'all': ['rater/css/styles.css']
+        }
+
+
+# Custimize display fields for Property model
+@admin.register(models.Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ['image', 'area', 'address', 'eircode', 'user', 'landlord']
+
+    def image(self, property):
+        return format_html('<img src="{}" class="thumbnail"/>',
+                           property.images.first().url)
+
+    class Media:
+        css = {
+            'all': ['rater/css/styles.css']
+        }
+
+
+# Custimize display fields for Review model
+@admin.register(models.Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['review', 'rating', 'user']
+
+
+# Custimize display fields for Reply model
+@admin.register(models.Reply)
+class ReplyAdmin(admin.ModelAdmin):
+    list_display = ['reply', 'user']
