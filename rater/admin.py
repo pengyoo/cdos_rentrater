1f1e158df6d1913dd7fe3998ab9925e353b6d681
Peng Yu <yp1990@gmail.com>
2024-03-30 09:39:14 +0000

    remove Trailing whitespace

--- rater/admin.py
+++ rater/admin.py
@@ -18,20 +18,15 @@ admin.site.register(models.Claim)
 @admin.register(models.Image)
 class ImageAdmin(admin.ModelAdmin):
     """ Custimize display fields for Image model """
-    
     list_display = ['title', 'image']
 
     def image(self, image):
-        
         """ display image """
-        
         return format_html('<img src="{}" class="thumbnail"/>',
                            image.url)
 
     class Media:
-        
         """ load style files """
-        
         css = {
             'all': ['rater/css/styles.css']
         }
@@ -40,21 +35,15 @@ class ImageAdmin(admin.ModelAdmin):
 
 @admin.register(models.Property)
 class PropertyAdmin(admin.ModelAdmin):
-    
     """ Custimize display fields for Property model """
-    
     list_display = ['image', 'area', 'address', 'eircode', 'user', 'landlord']
 
     def image(self, property):
-        
         """ display image """
-        
         return format_html('<img src="{}" class="thumbnail"/>',property.images.first().url)
 
     class Media:
-        
         """ load style files """
-        
         css = {
             'all': ['rater/css/styles.css']
         }
@@ -63,16 +52,12 @@ class PropertyAdmin(admin.ModelAdmin):
 
 @admin.register(models.Review)
 class ReviewAdmin(admin.ModelAdmin):
-    
     """ Custimize display fields for Review model """
-    
     list_display = ['review', 'rating', 'user']
 
 
 
 @admin.register(models.Reply)
 class ReplyAdmin(admin.ModelAdmin):
-    
     """ Custimize display fields for Reply model """
-    
     list_display = ['reply', 'user']
