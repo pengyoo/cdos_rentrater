b99005b8092a71ab44d19cca5153eb719c58edc7
Peng Yu <yp1990@gmail.com>
2024-04-05 16:11:03 +0000

    display property owner

--- rater/admin.py
+++ rater/admin.py
@@ -10,8 +10,6 @@ from . import models
 
 # Register models.
 admin.site.register(models.UserProfile)
-admin.site.register(models.Claim)
-
 
 
 @admin.register(models.Image)
@@ -52,7 +50,10 @@ class PropertyAdmin(admin.ModelAdmin):
 @admin.register(models.Review)
 class ReviewAdmin(admin.ModelAdmin):
     """ Custimize display fields for Review model """
-    list_display = ['review', 'rating', 'user']
+    list_display = ['rating', 'property_eircode', 'review', 'user', ]
+    
+    def property_eircode(self, review):
+        return review.property.eircode
 
 
 
@@ -60,3 +61,20 @@ class ReviewAdmin(admin.ModelAdmin):
 class ReplyAdmin(admin.ModelAdmin):
     """ Custimize display fields for Reply model """
     list_display = ['reply', 'user']
+
+
+
+@admin.register(models.Claim)
+class ClaimAdmin(admin.ModelAdmin):
+    """ Custimize display fields for Review model """
+    list_display = ['eircode', 'user', 'created_at', 'ownership_proof']
+    
+    def ownership_proof(self, property):
+        """ display image """
+        return format_html('<img src="{}" class="thumbnail"/>',property.images.first().url)
+
+    class Media:
+        """ load style files """
+        css = {
+            'all': ['rater/css/styles.css']
+        }
